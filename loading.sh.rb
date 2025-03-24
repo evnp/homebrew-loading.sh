1415179b53a4589f8645fd0dd4f17f2ce61b05f0
Evan Purcer <ep@e2.gg>
2025-03-24 09:55:52 -0700

    update sha and bin install command name

--- loading.sh.rb
+++ loading.sh.rb
@@ -2,15 +2,15 @@ class LoadingSh < Formula
   desc "loading.sh · Loading with style in the shell"
   homepage "https://github.com/evnp/loading.sh"
   url "https://github.com/evnp/loading.sh/archive/v0.0.2.tar.gz"
-  sha256 "47a7342e2dfd893d1fb3ee073ebf1cf5b67e92102d5fb034dfe10a87cc21a5d9"
+  sha256 "81028fcf7a197dc60fa3eec3afe294304d8e551b11db442e985e6754132ac6d7"
   license "MIT"
 
   def install
     man1.install "man/loading.sh.1"
-    bin.install "loading.sh"
+    bin.install "loading"
   end
 
   test do
-    assert_equal shell_output("#{bin}/loading.sh 10"), 0
+    assert_equal shell_output("#{bin}/loading 10"), 0
   end
 end
